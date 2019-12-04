0fad7e3605e573e8f717b2198609ea27277c4fcc
Léon Demeyere <leon.demeyere@hotmail.fr>
2019-12-04 16:58:00 +0100

    add userRestController

---
+++ src/main/java/fr/dawan/meepletown/controllers/UserRestController.java
@@ -0,0 +1,26 @@
+package fr.dawan.meepletown.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.dawan.meepletown.beans.User;
+import fr.dawan.meepletown.dao.GenericDao;
+
+@RestController
+@RequestMapping("userrest")
+public class UserRestController {
+	
+	@Autowired
+	GenericDao<User> dao;
+	
+	@GetMapping("test")
+	public List<User> getUser() {
+		
+		return dao.findAll(User.class);
+		
+	}
+}
